9324f02afeb87e6f6ecf606ed565724c61e404a3
sustainablelab <sustainablelab@gmail.com>
2020-12-29 11:43:51 -0500

    Block.draw_square has bool to randomize points

--- scripts/Block.gd
+++ scripts/Block.gd
@@ -7,22 +7,12 @@ var top_left: Vector2 setget set_top_left
 var length: float setget set_length
 var color: Color setget set_color
 
-# length_is_grow: true -> animate growing, false -> animate shrinking
-var length_is_grow: bool = true
-# growth_shrink_amount sets how much to grow/shrink by: 0 == no growth
-const GROW_SHRINK_AMOUNT: float = 0.1 # range: 0.0:1.0
-# TODO: clamp GROW_SHRINK_AMOUNT to the range 0 to 1
-var max_growth: float
-var max_shrink: float
-
 onready var grid: Grid = Grid.new()
 onready var smooth_move: Tween = Tween.new()
 
 func _ready():
 	add_child(grid)
 	self.length = grid.size
-	max_growth = grid.size * (1 + GROW_SHRINK_AMOUNT)
-	max_shrink = grid.size * (1 - GROW_SHRINK_AMOUNT)
 	add_child(smooth_move)
 
 
@@ -31,18 +21,10 @@ func _ready():
 # `delta` is fed to me from under the hood.
 # `delta` is on the order of 10ms, the exact value depends on how
 # busy the processor is.
+# Using `_delta` is a convention that means I do not depend on `delta`.
 func _process(delta):
-	# Vary the square size
-	# TODO: this is crude... do something better with Tween?
-	# Or don't do this at all. Vary polygon coordinates instead.
-	if length_is_grow:
-		self.length += delta * max_growth
-		if self.length >= max_growth:
-			length_is_grow = not length_is_grow
-	else:
-		self.length -= delta * max_shrink
-		if self.length <= max_shrink:
-			length_is_grow = not length_is_grow
+	var is_randomized: bool = true
+	draw_square(is_randomized)
 
 
 # _draw() runs once, sometime shortly after _ready() runs
@@ -93,17 +75,22 @@ func move(direction: Vector2 ) -> void:
 #					10.0,
 #					ColorN("lightsalmon", 1),
 #					)
-func draw_square() -> void:
+func draw_square(is_randomized: bool = false) -> void:
 	# Define the vertices
 	var points: PoolVector2Array = PoolVector2Array()
 	assert(points.empty()) # no points yet
-	points.append(self.top_left)
 	var x:float = self.top_left.x
 	var y:float = self.top_left.y
-	points.append(Vector2(x,y+self.length))
-	points.append(Vector2(x+self.length,y+self.length))
-	points.append(Vector2(x+self.length,y))
-	points.append(self.top_left)
+	if is_randomized:
+		points.append(Vector2(x,y))
+		points.append(Vector2(x,y+self.length))
+		points.append(Vector2(x+self.length,y+self.length))
+		points.append(Vector2(x+self.length,y))
+	else:
+		points.append(Vector2(x,y))
+		points.append(Vector2(x,y+self.length))
+		points.append(Vector2(x+self.length,y+self.length))
+		points.append(Vector2(x+self.length,y))
 	assert(not points.empty()) # got me some points
 
 	# Call built-in polygon drawing command
